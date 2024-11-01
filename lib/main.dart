748b427908248b3d1cf8d8b07af663b3d0524750
HFM007 <habib.fm20@student.unibi.ac.id>
2024-11-01 10:02:23 +0000

    Menambahkan gambar

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: FirstScreen(
-        
-      ),
+      home: FirstScreen(),
     );
   }
 }
+
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
 
@@ -27,7 +26,9 @@ class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: const BodyScreen(),
       appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
         leading: IconButton(
           icon: const Icon(Icons.menu),
           onPressed: () {
@@ -37,16 +38,17 @@ class _FirstScreenState extends State<FirstScreen> {
         title: const Text('First Screen'),
         actions: [
           IconButton(
-            onPressed: (){
+            onPressed: () {
               print('Search button clicked');
-            }, 
+            },
             icon: const Icon(Icons.search),
-            )
+          )
         ],
       ),
     );
   }
 }
+
 class BodyScreen extends StatefulWidget {
   const BodyScreen({super.key});
 
@@ -59,11 +61,62 @@ class _BodyScreenState extends State<BodyScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Title(
-          color: Colors.black, 
-          child: const Text('Farm House Lembang')
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            child: Image.asset('assets/images/Farmhouse-Lembang.jpg'),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 15),
+            child: const Text(
+              'Farm House Lembang',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(Icons.calendar_today_outlined),
+                    Text('Open Everyday'),
+                    SizedBox(
+                      height: 5,
+                      width: 5,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.access_time),
+                    Text('09:00 - 20:00'),
+                    SizedBox(
+                      height: 5,
+                      width: 5,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.monetization_on_outlined),
+                    Text('Rp 25.000'),
+                    SizedBox(
+                      height: 5,
+                      width: 5,
+                    ),
+                  ],
+                )
+              ],
+            ),
           )
-        ),
+        ],
+      )),
     );
   }
 }
