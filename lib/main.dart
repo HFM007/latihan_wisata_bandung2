4bd8b0bb4abf80c081acbffd6d5009172cca2394
HFM007 <habib.fm20@student.unibi.ac.id>
2024-11-01 10:46:21 +0000

    Menambahkan galeri gambar di bawah deskripsi

--- lib/main.dart
+++ lib/main.dart
@@ -31,16 +31,12 @@ class _FirstScreenState extends State<FirstScreen> {
         backgroundColor: Colors.blueAccent,
         leading: IconButton(
           icon: const Icon(Icons.menu),
-          onPressed: () {
-            print('Menu button clicked');
-          },
+          onPressed: () {},
         ),
         title: const Text('First Screen'),
         actions: [
           IconButton(
-            onPressed: () {
-              print('Search button clicked');
-            },
+            onPressed: () {},
             icon: const Icon(Icons.search),
           )
         ],
@@ -59,64 +55,89 @@ class BodyScreen extends StatefulWidget {
 class _BodyScreenState extends State<BodyScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            child: Image.asset('assets/images/Farmhouse-Lembang.jpg'),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 15),
-            child: const Text(
-              'Farm House Lembang',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'assets/images/Farmhouse-Lembang.jpg',
+              height: 200,
+              width: double.infinity, // Membuat gambar menyesuaikan lebar layar
+              fit: BoxFit
+                  .cover, // Menyesuaikan gambar agar memenuhi lebar dan tinggi kontainer
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 15),
+              child: const Text(
+                'Farm House Lembang',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Container(
-            child: const Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Column(
                   children: [
                     Icon(Icons.calendar_today_outlined),
                     Text('Open Everyday'),
-                    SizedBox(
-                      height: 5,
-                      width: 5,
-                    ),
+                    SizedBox(height: 5),
                   ],
                 ),
                 Column(
                   children: [
                     Icon(Icons.access_time),
                     Text('09:00 - 20:00'),
-                    SizedBox(
-                      height: 5,
-                      width: 5,
-                    ),
+                    SizedBox(height: 5),
                   ],
                 ),
                 Column(
                   children: [
                     Icon(Icons.monetization_on_outlined),
                     Text('Rp 25.000'),
-                    SizedBox(
-                      height: 5,
-                      width: 5,
-                    ),
+                    SizedBox(height: 5),
                   ],
-                )
+                ),
               ],
             ),
-          )
-        ],
-      )),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: const Text(
+                "Berada di jalur utama Bandung-Lembang, Farmhouse Lembang adalah tempat wisata yang sangat populer. Tempat ini memiliki desain arsitektur ala Eropa yang sangat kental. Pengunjung bisa berfoto-foto di spot-spot menarik yang ada di sini.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
